82607a177735ffb120e29e46608fecf19c8dea8a
Cameron <cameron.milliken@gmail.com>
2019-02-07 13:44:27 -0700

    adds rep and state models

---
+++ Representative/Models/Representative.swift
@@ -0,0 +1,23 @@
+//
+//  Representative.swift
+//  Representative
+//
+//  Created by Cameron Milliken on 2/7/19.
+//  Copyright © 2019 Cameron Milliken. All rights reserved.
+//
+
+import Foundation
+
+struct TopLevelDictionary: Decodable {
+    let results: [Representative]
+}
+
+struct Representative: Decodable {
+    let name: String
+    let party: String
+    let state: String
+    let district: String
+    let phone: String
+    let office: String
+    let link: String
+}
---
+++ Representative/Models/States.swift
@@ -0,0 +1,13 @@
+//
+//  States.swift
+//  Representative
+//
+//  Created by Cameron Milliken on 2/7/19.
+//  Copyright © 2019 Cameron Milliken. All rights reserved.
+//
+
+import Foundation
+
+enum States {
+    static var allStates = ["AK", "AL", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+}
